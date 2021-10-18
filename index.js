e095c0910f51db082d004605246d0f5fb5e443dd
tsoriLache <89573774+tsoriLache@users.noreply.github.com>
2021-10-18 13:10:43 +0300

    added func

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 let moment = require('moment');
 function timeToNow(dateArr){
     let now =moment();
+    console.log(now)
     return now.diff(dateArr,'minutes'); 
-    
 }
 
-console.log(timeToNow([2007, 5, 29]))
+function  TimeFromAToB(date1,date2){
+    return moment(date1).diff(date2,'minutes');
+}
+console.log(timeToNow([2008, 5, 29]))
+console.log(TimeFromAToB([2007, 5, 29],[2007, 3, 29]))
