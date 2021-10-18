8d4d6ec2c1d5d65fd19d58974ffeec2bc0867247
tsoriLache <89573774+tsoriLache@users.noreply.github.com>
2021-10-18 15:14:08 +0300

    last commit
    fghd

--- index.js
+++ index.js
@@ -8,5 +8,13 @@ function timeToNow(dateArr){
 function  TimeFromAToB(date1,date2){
     return moment(date1).diff(date2,'minutes');
 }
-console.log(timeToNow([2008, 5, 29]))
-console.log(TimeFromAToB([2007, 5, 29],[2007, 3, 29]))
+
+function daysInMonth(date,format){
+    const dateObj =  moment(date,format).toObject();
+    console.log(`${dateObj.years}-${dateObj.months}`)
+    // ,dateObj,moment(`${dateObj.years}-${dateObj.months}`,"YYYY-MM").daysInMonth()) 
+
+}
+// console.log(timeToNow([2008, 5, 29]))
+// console.log(TimeFromAToB([2007, 5, 29],[2007, 3, 29]))
+console.log(daysInMonth("02-25-1995", "MM-DD-YYYY"))
